854a2c3f90a7d83340f4f68fa4a2cf6213db2c80
Bjørn Harbo Dahle <bdahle@hey.com>
2021-05-27 14:28:18 +0200

    refactor, code order

--- app.js
+++ app.js
@@ -15,17 +15,6 @@ function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
 }
-
-addBookToLibrary(
-  "Harry Potter and the Philosopher's Stone",
-  "J.K. Rowling",
-  309,
-  "READ"
-);
-addBookToLibrary("Tools of Titans", "Timothy Ferriss", 707, "READ");
-
-let table = document.querySelector("table");
-
 function clearTableBody() {
   for (var i = table.rows.length - 1; i > 0; i--) {
     table.deleteRow(i);
@@ -50,14 +39,23 @@ function addCell(row, cellText) {
   cell.appendChild(text);
 }
 
-updateTable();
-
 const titleInput = document.querySelector("#title");
 const authorInput = document.querySelector("#author");
-
 const newBookForm = document.querySelector(".new-book-form");
+let table = document.querySelector("table");
+
 newBookForm.addEventListener("submit", (e) => {
   e.preventDefault(); //prevent reloading the page
   addBookToLibrary(titleInput.value, authorInput.value, 707, "READ");
   updateTable();
 });
+
+addBookToLibrary(
+  "Harry Potter and the Philosopher's Stone",
+  "J.K. Rowling",
+  309,
+  "READ"
+);
+addBookToLibrary("Tools of Titans", "Timothy Ferriss", 707, "READ");
+
+updateTable();
